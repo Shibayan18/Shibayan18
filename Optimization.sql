265021720e3a604d6debce36dc0269122d3ca089
Shibayan18 <80824558+Shibayan18@users.noreply.github.com>
2021-03-30 11:26:50 +0000

    Add files via upload

---
+++ Optimization.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [optimization].[BatteryInstructions]
+ (
+    [DistributedEnergyResourceSK] INT            IDENTITY (1, 1) NOT NULL,
+    [DeviceId]                    NVARCHAR (200) NOT NULL,
+    [Name]                        NVARCHAR (200) NOT NULL,
+    [Simulated]                   BIT            NOT NULL,
+    [MadeatDateTime]         DATETIME2      NOT NULL,
+    [StartDateTime]         DATETIME2      NOT NULL,
+    [EndDateTime]         DATETIME2      NOT NULL,
+    [GridCarbonEmissionSK]          INT             IDENTITY (1, 1) NOT NULL,
+    [MarginalCO2Intensity_Forcast_gCO2kWh] DECIMAL(10, 6) NULL,
+    [Instruction_Issued]  INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([DistributedEnergyResourceSK] ASC)
+);
+GO
+
+
+CREATE SCHEMA [optimization] 
